f3337f3a2806ea02916f95f97825ab25195d3ba6
lllisteu <lllisteu@users.noreply.github.com>
2022-08-11 10:41:50 +0200

    Refactor ls, Base#ls

--- lib/uio/ls.rb
+++ lib/uio/ls.rb
@@ -3,17 +3,24 @@ require 'uio/search'
 
 module UIO
 
+  class Base
+
+    def ls
+      puts "%s%s%s %14.14s %.60s" % [
+        ( ! content.empty?             ? 'c' : ' ' ),
+        ( data['link']                 ? 'l' : ' ' ),
+        ( (data['lat'] or data['lon']) ? 'g' : ' ' ),
+        data['uid'],
+        title
+      ]
+    end
+
+  end
+
   class Collection
 
     def ls(regex=nil)
-      ( regex ? search(regex) : self ).each do |i|
-        puts "%s%s %14.14s %.60s" % [
-          ( i.data['link']     ? 'l' : ' ' ),
-          ( ! i.content.empty? ? 'c' : ' ' ),
-          i['uid'],
-          i.title
-        ]
-      end
+      ( regex ? search(regex) : self ).each &:ls
     end
 
   end
