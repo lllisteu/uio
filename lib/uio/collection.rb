e49ef42ba376af0abb38f0b437c56d505e285a0f
lllisteu <lllisteu@users.noreply.github.com>
2025-12-30 10:36:17 +0100

    rm glob variants

--- lib/uio/collection.rb
+++ lib/uio/collection.rb
@@ -8,39 +8,12 @@ module UIO
       Collection.new
     end
 
-    def glob(pattern)
-      Collection.glob pattern
-    end
-
-    def dir(directory=Dir.pwd)
-      glob "#{directory}/**/*.{txt,md,markdown}"
-    end
-
   end
 
 
   class Collection < Array
 
     include UIO::Shared::Log
-
-    class << self
-
-      def glob(pattern)
-        new glob: pattern
-      end
-
-    end
-
-
-    def initialize(glob: nil)
-      self.glob glob if glob
-    end
-
-    def glob(pattern)
-      Dir.glob(pattern).sort.each do |file|
-        push UIO.file(file)
-      end.size
-    end
 
     def inspect
       "#{self.class} (%d)" % size
